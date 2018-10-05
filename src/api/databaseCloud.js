3da8807f13fbf687c70a5dee8b20d98e18ab6acd
yangk <614044401@qq.com>
2018-10-05 10:25:10 +0800

    submit

---
+++ src/api/databaseCloud.js
@@ -0,0 +1,12 @@
+import config from '@/config'
+// 1. 获取数据库引用
+const db = wx.cloud.database({ env: config.database.env })
+// 2. 构造查询语句
+// collection 方法获取一个集合的引用
+// where 方法传入一个对象，数据库返回集合中字段等于指定值的 JSON 文档。API 也支持高级的查询条件（比如大于、小于、in 等），具体见文档查看支持列表
+// get 方法会触发网络请求，往数据库取数据
+const test = db.collection('test')
+export default {
+  db,
+  test
+}
